5a96e8f6f4aeada5013e68d6f23fac3b1ac22779
qun.zheng <zhengqun@youedata.com>
2018-11-22 17:50:59 +0800

    初始导入

---
+++ licensing-service/src/main/java/com/stone/learn/licences/Application.java
@@ -0,0 +1,12 @@
+package com.stone.learn.licences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ licensing-service/src/main/java/com/stone/learn/licences/controllers/LicenseServiceController.java
@@ -0,0 +1,26 @@
+package com.stone.learn.licences.controllers;
+
+import com.stone.learn.licences.model.License;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author qun.zheng
+ * @create 2018/11/22
+ **/
+@RestController
+@RequestMapping("/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @GetMapping("/{licenseId}")
+    public License getLicenses(@PathVariable("organizationId") String organizationId,
+                               @PathVariable("licenseId") String licenseId) {
+        return new License()
+                .withId(licenseId).withOrganizationId(organizationId)
+                .withLicenseType("Seat").withProductName("Teleco");
+    }
+}
